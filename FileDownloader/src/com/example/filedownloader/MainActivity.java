4612ad705913c86c6ec10b69648508497db7abcf
Posashkov Dmitry <k0kak0la1488@gmail.com>
2014-04-10 21:32:31 +0400

    Try to open with default gallery.

--- FileDownloader/src/com/example/filedownloader/MainActivity.java
+++ FileDownloader/src/com/example/filedownloader/MainActivity.java
@@ -8,15 +8,17 @@ import java.net.URLConnection;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -91,11 +93,21 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 				String imagePath = getFilesDir().toString() + "/"
 						+ DOWNLOADED_IMAGE_NAME;
-				imageDownloaded.setImageDrawable(Drawable
-						.createFromPath(imagePath));
+				// imageDownloaded.setImageDrawable(Drawable
+				// .createFromPath(imagePath));
+
+				Intent intent = new Intent();
+				intent.setAction(Intent.ACTION_VIEW);
+				intent.setDataAndType(Uri.parse(imagePath), "image/*");
+				startActivity(intent);
+
+				// startActivity(new Intent(Intent.ACTION_VIEW, Uri
+				// .parse(imagePath)));
+
 				buttonAction.setVisibility(4);
 				textViewStatus.setVisibility(4);
 				progressBarTimer.setVisibility(4);
+
 			}
 		});
 	}
@@ -156,7 +168,8 @@ public class MainActivity extends Activity {
 				input.close();
 
 			} catch (Exception e) {
-				Log.e("Error: ", e.getMessage());
+				Toast.makeText(getApplicationContext(), "Error.",
+						Toast.LENGTH_LONG).show();
 			}
 
 			return null;
