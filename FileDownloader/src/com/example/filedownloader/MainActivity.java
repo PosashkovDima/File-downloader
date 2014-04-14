c7b545eda3f26e0f9659bf349322bea806e89d09
Posashkov Dmitry <k0kak0la1488@gmail.com>
2014-04-14 23:52:45 +0400

    Add Toast.

--- FileDownloader/src/com/example/filedownloader/MainActivity.java
+++ FileDownloader/src/com/example/filedownloader/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -38,6 +39,8 @@ public class MainActivity extends ActionBarActivity {
 					setStatusDownloaded();
 					setOnClickeListnerOpen();
 				} else {
+					Toast.makeText(MainActivity.this, "Error. Try again.",
+							Toast.LENGTH_SHORT).show();
 					setStatusIdle();
 				}
 			}
